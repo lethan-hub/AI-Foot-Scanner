df844653ac2a803e1d20ecaee6d2d28c1d15bc21
lethan-hub <190683211+lethan-hub@users.noreply.github.com>
2026-07-08 16:13:29 -0500

    Created Main Files
    - Pushed code from the past few days
    - Created a main.py file, which holds functions that loads the images, processes and runs
    - Debugged some errors with channels of grayscale and BGR
    - Test ran code, but need to find medium to distinguish the background to be gone and to be more focused on the foot

---
+++ main.py
@@ -0,0 +1,64 @@
+# cv2 is the installation of Computer Vision that is used for image processing, video analysis and machine learning
+import cv2
+# numpy as np imports the entire numpy library and then it creates np as a nickname
+import numpy as np
+
+
+
+# Created a function called load_image with a paramter called path 
+def load_image(path):
+
+# Created a variable called img
+    # img holds the value of the library call of cv2, which uses computer vision
+    # .imread is a function that is used in Python to read a image file into storage, which allows numpy to convert it into a numerical array so it can use it
+    # Then the parameter called path is used as the image
+    img = cv2.imread(path)
+
+# If statement to check if there is no image uploaded it will print an error message and return None
+    if img is None:
+        print("Error: Image file not found!")
+        return None
+    # No matter what happens it will return img
+    return img
+
+# Function called process_images that converts the img into black n white and blurs it out
+# Gray scale, Gaussian Blur, and Canny
+def process_edges(color_image):
+
+    # Variable that changes the color of the image to gray
+    gray = cv2.cvtColor(color_image,cv2.COLOR_BGR2GRAY)
+
+
+    # Variable called blurred that equals cv2.GaussianBlur, which blurs the uncessasary details like the floor background or textures
+    blurred = cv2.GaussianBlur(gray,(5,5),0)
+
+    # Variable called edges that uses .Canny, which allows the edges by determing the major brightness difference
+    edges = cv2.Canny(blurred,100,250)
+
+    return edges
+
+
+# Main Function
+
+if __name__ == "__main__":
+
+    # Variable called raw_image that runs the load_image function with the value of test_foot.jpg in it
+    raw_image = load_image("test_foot.jpg")
+
+    
+    # Checks if raw_image is not None, which means if it has a value, continue
+    if raw_image is not None:
+
+
+        # Extract edges
+        # It runs the process_edges function on the raw_image
+        detected_edges = process_edges(raw_image)
+
+        # Render
+        # .imshow shows the image into a 2D array or pixels
+        cv2.imshow("Detected Edges: ",detected_edges)
+
+
+        # Keep open until until told so
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
